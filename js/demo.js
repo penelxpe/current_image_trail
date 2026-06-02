9569d3ec32156dcfdc88c6f81104c9aa38e2bc4d
penelxpe <penelope.perret123@gmail.com>
2026-06-02 21:29:19 +0200

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -110,9 +110,7 @@
             // zIndex value to apply to the upcoming image
             this.zIndexVal = 1;
             // mouse distance required to show the next image
-            this.threshold = window.matchMedia('(pointer: coarse)').matches ?
-                35 :
-                100;
+            this.threshold = 15;
             // render the images
             requestAnimationFrame(() => this.render());
         }
